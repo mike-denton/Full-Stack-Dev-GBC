fbe84895b35f58d012889e3305f49fc33c97983c
unknown <mike.denton.work@gmail.com>
2021-01-24 21:27:46 -0500

    upload lab solutions

---
+++ Full-Stack-1/Session 6 - JS Intro -Data Types/Solutions/Exercise 5/custom.js
@@ -0,0 +1,13 @@
+
+
+var pi = Math.PI;
+
+console.log("This value of PI is " + pi);
+console.log("The value of PI rounded is " + Math.round(pi));
+console.log("The value of PI floor is " + Math.floor(pi));
+console.log("The value of PI ceiling is " + Math.ceil(pi));
+
+console.log("This square root of PI is " + Math.sqrt(pi));
+console.log("PI to the power of 2 is " + Math.pow(pi, 2));
+
+console.log("This is a random number: " + Math.random());
