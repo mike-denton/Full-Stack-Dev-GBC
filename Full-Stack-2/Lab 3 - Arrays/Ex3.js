621c4bcb22c7c5f292d5ec0325e612c81b376e4a
unknown <mike.denton.work@gmail.com>
2021-02-16 13:38:18 -0500

    added FS II lab solutions

---
+++ Full-Stack-2/Lab 3 - Arrays/Ex3.js
@@ -0,0 +1,12 @@
+
+
+function reverseIt(myArr) {
+	return myArr.reverse();
+}
+
+console.log(reverseIt([1, 2, 3, 4]))// [4, 3, 2, 1]
+
+reverseIt([9, 9, 2, 3, 4]) // [4, 3, 2, 9, 9]
+
+reverseIt([]) // []
+
