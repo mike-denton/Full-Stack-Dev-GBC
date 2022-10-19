8782d4e448eaf0230aa6e4ed750e390dc8cce2f6
Mike Denton <MDenton@corrosionservice.com>
2022-10-19 01:03:59 -0400

    update for Fall 2022

--- Full-Stack-3/Session 7 - Lab Test/1-Calculator/app.js
+++
@@ -1,7 +0,0 @@
-var calc = require('./calculator');
-
-console.log('multiply 6 * 6 equals: ' + calc.multiplyTwoNumbers(6, 6));
-console.log('even doubler 4 equals: ' + calc.evenDoubler(4))
-
-console.error('multiply true * false equals: ' + calc.multiplyTwoNumbers(true, false));
-console.log('even doubler [] equals: ' + calc.evenDoubler([]))
---
+++ Full-Stack-I-Front-End/Session 2 - Intro to React/Demo/Demo-Lecture-ES6-1/Demo/2.0-array-methods.js
@@ -0,0 +1,24 @@
+
+const fruits = ['Apples', 'Oranges', 'Grapes'];
+
+
+// FOREACH
+fruits.forEach((fruit, index) => console.log(fruit));
+
+// MAP
+var upperCaseFruit = function(fruit) {
+  return fruit.slice(0, -1).toUpperCase();
+}
+const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
+console.log(singleFruit);
+
+// FILTER
+const people = [
+  { id: 1, name: 'Karen' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Sharon' }
+];
+
+// filter used commonly in Redux, when deleting something from the state
+const people2 = people.filter(person => person.id !== 2);
+console.log(people2);
